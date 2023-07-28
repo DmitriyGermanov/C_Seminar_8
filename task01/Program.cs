ada61918ddf2e93e67d265d3dd7e4fc55b4c3de2
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-28 22:41:59 +0300

    added second task

---
+++ task01/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(10, 100);
+        }
+
+    }
+    return result;
+}
+
+int[,] Change(int[,] array)
+{
+int[,] result = new int[array.GetLength(1), array.GetLength(0)];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j=0; j< array.GetLength(1); j++)
+        {
+        result[j, i] = array[i, j];
+        }
+    }  
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = GetArray(5, 6);
+PrintArray(array);
+array = Change(array);
+System.Console.WriteLine();
+PrintArray(array);
